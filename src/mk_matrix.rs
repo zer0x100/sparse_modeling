9dc8e4577dafd2eae12ca73566f4a5673dbdffad
unknown <vikingzer0x100@gmail.com>
2023-10-14 03:27:15 +0900

    change simulations condition

--- src/mk_matrix.rs
+++ src/mk_matrix.rs
@@ -4,19 +4,13 @@ use crate::prelude::*;
 //FFTするわけではないのでサイズは2^(整数)じゃなくていい
 #[allow(dead_code)]
 pub fn mk_dft_mat(n: usize) -> Array2<c64> {
-    let mut ft_matrix: Array2<c64> = ndarray_linalg::random((n, n));
-
     let theta = -2. * PI / n as f64;
-    for i in 0..n {
-        for j in 0..n {
-            ft_matrix[[i, j]] = c64 {
-                re: f64::cos(theta * i as f64 * j as f64),
-                im: f64::sin(theta * i as f64 * j as f64),
-            } / (n as f64).sqrt();
+    ArrayBase::from_shape_fn((n, n), |(i, j)| 
+        c64 {
+            re: f64::cos(theta * i as f64 * j as f64),
+            im: f64::sin(theta * i as f64 * j as f64),
         }
-    }
-
-    ft_matrix
+    )
 }
 
 #[allow(dead_code)]
